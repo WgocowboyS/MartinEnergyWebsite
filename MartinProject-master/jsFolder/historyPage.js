d7f222e078afbeb967a7f1a45cb256b5be207cc3
WgocowboyS <wgocowboys@gmail.com>
2023-06-13 12:41:45 -0400

    implemented one module to many videos

--- MartinProject-master/jsFolder/historyPage.js
+++ MartinProject-master/jsFolder/historyPage.js
@@ -1,20 +1,20 @@
-function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-    for (var i = 0; i < reveals.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
-        if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add("active");
-        } else {
-            reveals[i].classList.remove("active");
-        }
-    }
-}
-
-window.addEventListener("scroll", reveal);
-
-// To check the scroll position on page load
-reveal();
-
-
+document.addEventListener("DOMContentLoaded", function() {
+    // On button click, populate video src attribute with video path
+    document.querySelectorAll('.video-btn').forEach(function(videoBtn) {
+      videoBtn.addEventListener('click', function() {
+        var video = this.getAttribute('data-video');
+        var videoTag = document.getElementById('video');
+        videoTag.src = video;
+        //console.log(video);
+        //console.log(videoTag.src);
+      });
+    });
+  
+    // When modal is hidden, remove video src attribute to stop video
+    var videoModal = document.getElementById('historyVideo');
+    videoModal.addEventListener('hidden.bs.modal', function() {
+      var videoTag = document.getElementById('video');
+      videoTag.pause();
+      videoTag.src = '';
+    });
+  });
